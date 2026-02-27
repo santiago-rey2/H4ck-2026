f42c9f865ce19423f614e5330d4facb9e6a95ddd
Pablo Ulloa Santín <pablo.ulloa.santin@udc.es>
2026-02-27 23:41:13 +0100

    feat: add initial configuration and components for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="mx-auto max-w-5xl p-8 text-center">
+			<div></div>
+			<h1 className="text-5xl font-bold leading-tight">Vite + React</h1>
+			<div className="p-8">
+				<button
+					type="button"
+					className="cursor-pointer rounded-lg border border-transparent bg-neutral-900 px-5 py-2.5 text-base font-medium transition-colors hover:border-indigo-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-400 dark:bg-neutral-900 light:bg-gray-100"
+					onClick={() => setCount((count) => count + 1)}
+				>
+					count is {count}
+				</button>
+				<p className="mt-4">
+					Edit <code className="font-mono">src/App.tsx</code> and save to test
+					HMR
+				</p>
+			</div>
+			<p className="text-gray-400">
+				Click on the Vite and React logos to learn more
+			</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
